f79dabb1e80772b293064f95a203ad72115f82bc
Administrator <Administrator@Gitetsu-PC>
2014-09-02 13:26:14 +0900

    change source

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: 2014Äê9ÔÂ2ÈÕ
+ *      Author: Administrator
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include "mfrc522.h"
+int main(void){
+    //Recognized card ID
+    uint8_t CardID[5];
+    //My cards id
+    //I read them with program below, and write this here
+    uint8_t MyID[5] = {
+        0x43, 0xdc, 0x52, 0xb6, 0x7b    //My card on my keys
+    };
+    char buffer[50];
+
+    while (1) {
+        //If any card detected
+        if (MFRC522_Check(CardID) == MI_OK) {
+            puts("Card detected    ");
+            printf("0x%02x\n0x%02x\n0x%02x\n0x%02x\n0x%02x", CardID[0], CardID[1], CardID[2], CardID[3], CardID[4]);
+        } else {
+            //Some printing to delete content
+            puts("Card not detected");
+        }
+    }
+	return 0;
+}
